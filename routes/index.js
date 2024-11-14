c0e6a3c9d631746b4b8c5d9392690ab695e58e32
Zyema <ablyazova.z.i23@gmail.com>
2024-11-14 21:26:12 +0300

    4_2

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/horrors', function(req, res, next) {
+  res.send("<h1>Ужасы - фильмы</h1>")
+  });
+
+router.get('/comedy', function(req, res, next) {
+    res.send("<h1>Комедии - фильмы</h1>")
+    });
+    
+router.get('/ ', function(req, res, next) {
+  res.send("<h1>Драмы - фильмы</h1>")
+  });
+    
+
 module.exports = router;
