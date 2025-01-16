22ac773b65091ca71da00f290d9e4d90c9d0f098
naonaogh <152014650+naonaogh@users.noreply.github.com>
2025-01-16 19:49:56 +0300

    7_3

--- testMongoose.js
+++ testMongoose.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-var schema = mongoose.Schema({ name: String })
-schema.methods.meow = function(){
-console.log(this.name + " сказал мяу")
-}
-const Cat = mongoose.model('Cat', schema);
-const kitty = new Cat({ name: 'Пушок' });
-kitty.save().then(() => kitty.meow());
+var Film = require('./models/film.js').film
+var newfilm = new Film({
+title: "Сияние",
+nick: 'Shining'
+})
+newfilm.save();
