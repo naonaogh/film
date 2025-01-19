551a88fe4e6cbdff09a6c804e92a6faeb02b46d1
naonaogh <152014650+naonaogh@users.noreply.github.com>
2025-01-19 22:00:15 +0300

    fin

--- testMongoose.js
+++ testMongoose.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-var Film = require('./models/film.js').film
-var newfilm = new Film({
-title: "Сияние",
-nick: 'Shining'
-})
-newfilm.save();
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
+// var Film = require('./models/film.js').film
+// var newfilm = new Film({
+// title: "Сияние",
+// nick: 'Shining'
+// })
+// newfilm.save();
